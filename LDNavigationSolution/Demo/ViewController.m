0ff3a4b2f85502b0d91bd22203445b420c7f4c27
Dennis <leoli@useus.cn>
2017-03-19 15:18:53 +0800

    初始化工程

---
+++ LDNavigationSolution/Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LDNavigationSolution
+//
+//  Created by Dennis on 17/03/2017.
+//  Copyright © 2017 Dennis Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ LDNavigationSolution/Supporting Files/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LDNavigationSolution
+//
+//  Created by Dennis on 17/03/2017.
+//  Copyright © 2017 Dennis Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
