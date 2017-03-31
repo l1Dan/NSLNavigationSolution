976d6b7b42e2257c68608c99fc41024a95a1de74
Dennis <leoli@useus.cn>
2017-03-31 17:50:41 +0800

    添加Carthage支持

---
+++ NSLNavigationSolution/NSLNavigationSolution.h
@@ -0,0 +1,19 @@
+//
+//  NSLNavigationSolution.h
+//  NSLNavigationSolution
+//
+//  Created by Dennis on 31/03/2017.
+//  Copyright © 2017 Dennis Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSLNavigationSolution.
+FOUNDATION_EXPORT double NSLNavigationSolutionVersionNumber;
+
+//! Project version string for NSLNavigationSolution.
+FOUNDATION_EXPORT const unsigned char NSLNavigationSolutionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSLNavigationSolution/PublicHeader.h>
+
+
