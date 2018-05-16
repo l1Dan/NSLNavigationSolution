f3c85fce2678efe2ca0d3e1bb39d069cd4f9839c
李丹 <lidan@onething.com>
2018-05-16 16:18:32 +0800

    update: 添加注释

--- NSLNavigationSolution/Source/NSLNavigationSolutionProtocol.h
+++ NSLNavigationSolution/Source/NSLNavigationSolutionProtocol.h
@@ -9,6 +9,26 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
+
+/**
+ UINavigationControllerCustomizable
+ */
+@protocol UINavigationControllerCustomizable <NSObject>
+
+/**
+ 当手势滑动或点击返回按钮时调用
+ 
+ @param navigationController 当前导航控制器
+ @param usingGesture 是否使用手势滑动：区分是手势滑动还是点击返回按钮
+ @return 是否继续完成手势滑动或点击按钮操作
+ */
+- (BOOL)navigationController:(UINavigationController *)navigationController shouldJumpToViewControllerUsingGesture:(BOOL)usingGesture;
+
+@end
+
+/**
+ NSLViewControllerProtocol
+ */
 @protocol NSLViewControllerProtocol <NSObject>
 
 /// 是否禁用手势返回
@@ -19,6 +39,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
+/**
+ NSLNavigationControllerProtocol
+ */
 @protocol NSLNavigationControllerProtocol <NSObject>
 
 /// 设置需要跳转的控制器
--- NSLNavigationSolution/Source/UINavigationController+NSLNavigationSolution.h
+++ NSLNavigationSolution/Source/UINavigationController+NSLNavigationSolution.h
@@ -10,27 +10,24 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@protocol UINavigationControllerCustomizable <NSObject>
-
-/**
- 当手势滑动或点击返回按钮时调用
-
- @param navigationController 当前导航控制器
- @param usingGesture 是否使用手势滑动：区分是手势滑动还是点击返回按钮
- @return 是否继续完成手势滑动或点击按钮操作
- */
-- (BOOL)navigationController:(UINavigationController *)navigationController shouldJumpToViewControllerUsingGesture:(BOOL)usingGesture;
-
-@end
-
 @interface UIViewController (NSLNavigationSolution)
 
+/**
+ 提供方法：
+ *  是否禁用手势返回
+ *  导航栏是否透明
+ */
 @property (nonatomic, strong, readonly) id<NSLViewControllerProtocol> solution;
 
 @end
 
 @interface UINavigationController (NSLNavigationSolution)
 
+/**
+ 提供方法：
+ *  跳转控制器
+ *  点击系统返回操作
+ */
 @property (nonatomic, strong, readonly) id<NSLNavigationControllerProtocol> solution;
 
 @end
